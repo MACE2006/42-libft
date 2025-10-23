1133f383dd3cd2b72f4d19509a84de05802e7079
Callum Vorley <cvorley@c1r4s2.42london.com>
2025-10-23 12:04:59 +0100

    added ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvorley <cvorley@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/23 12:00:01 by cvorley           #+#    #+#             */
+/*   Updated: 2025/10/23 12:04:36 by cvorley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *pstr)
+{
+	int	sum;
+	int	operator;
+
+	sum = 0;
+	operator = 1;
+	while (*pstr == ' ' || (*pstr >= 9 && *pstr <= 13))
+		pstr++;
+	while (*pstr == '-' || *pstr == '+')
+	{
+		if (*pstr == '-')
+			operator *= -1;
+		pstr++;
+	}
+	while (*pstr >= '0' && *pstr <= '9')
+	{
+		sum = (sum * 10) + (*pstr - '0');
+		pstr++;
+	}
+	return (operator * sum);
+}
