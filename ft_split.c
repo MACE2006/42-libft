542a88fd90e9cc4d6f7edea06d411278dd3edb7b
MACE2006 <cvorley06@gmail.com>
2025-10-24 10:28:39 +0100

    Added ft_strmapi

--- ft_split.c
+++ ft_split.c
@@ -11,8 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 static int	ft_count_words(char const *s, char c)
 {
--- libft.h
+++ libft.h
@@ -15,6 +15,7 @@
 
 # include <stdio.h>
 # include <stdlib.h>
+# include <stddef.h>
 
 typedef struct s_list
 {
