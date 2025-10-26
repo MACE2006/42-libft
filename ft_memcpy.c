f431dfe0c84bffaf1037cf1be083dfa7a63e9b08
MACE2006 <cvorley06@gmail.com>
2025-10-26 09:58:10 +0000

    Fixed calloc and memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -18,7 +18,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*psrc;
 
 	if (!dest || !src)
-		return (NULL);
+		return (dest);
 	pdest = (unsigned char *)dest;
 	psrc = (unsigned char *)src;
 	while (n--)
